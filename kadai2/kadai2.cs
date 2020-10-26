7613c7066d376f37188df8bafa820f18025bbc2b
raki278 <09087425890001azusa@gmail.com>
2020-10-26 16:49:37 +0900

    補修課題1
    単位の補修の課題

---
+++ kadai2/kadai2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Kadai2 : Form
+{
+    private Label lb;
+
+
+
+    public static void Main()
+    {
+        Application.Run(new Kadai2());
+    }
+    public Kadai2()
+    {
+        this.Text = "課題です。";
+        this.Width = 450; this.Height = 300;
+
+        lb = new Label();
+        lb.Text = "カーソルをこの枠に入れてください。";
+
+        lb.Parent = this;
+
+        this.MouseEnter += new EventHandler(fm_MouseEnter);
+        this.MouseLeave += new EventHandler(fm_MouseLeave);
+
+    }
+    public void fm_MouseEnter(Object sender, EventArgs e)
+    {
+        lb.Text = "参考書を";
+    }
+    public void fm_MouseLeave(Object sender, EventArgs e)
+    {
+        lb.Text = "見ながらやりました。";
+
+    }
+}
+  
